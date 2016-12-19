9b79e3d44478ba3a2778a3ebe8b42c3cc4734de6
zrh <459676797@qq.com>
2016-12-19 17:08:23 +0800

    51job

--- job_spider/view.py
+++ job_spider/view.py
@@ -1,15 +1,15 @@
 from django.http import HttpResponse
 from spider import job_51job
-com = '百度在线网络技术（北京）有限公司'
+# com = '百度在线网络技术（北京）有限公司'
 
 
 def hello(request):
-    # if request.GET:
-    #     data = {}
-        # pass
-        # data['company'] = request.POST['company']
-        # job_51job.spider_job(data['company'])
-    job_51job.spider_job(com)
+    if request.POST:
+        data = {}
+        pass
+        data['company'] = request.POST['company']
+        job_51job.spider_job(data['company'])
+    # job_51job.spider_job(com)
     return HttpResponse("Hello world!")
 
 
