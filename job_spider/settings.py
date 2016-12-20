1d50995cf717409c01817630644627205ee55079
zrh <459676797@qq.com>
2016-12-20 11:29:35 +0800

    51job

--- job_spider/settings.py
+++ job_spider/settings.py
@@ -72,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'job_spider.wsgi.application'
 
-CORS_ORIGIN_ALLOW_ALL = True
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -121,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
